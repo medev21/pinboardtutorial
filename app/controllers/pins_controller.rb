3c2dc030fa00c08804ae2f7ed0d5631c6aa7ca58
Martin Ben <mebenav88@gmail.com>
2015-11-04 16:14:55 -0500

    add show action in pin controller, show view, modify application html haml

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,4 +1,5 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
   def index
   end
 
@@ -8,11 +9,24 @@ class PinsController < ApplicationController
 
   def create
     @pin = Pin.new(pin_params)
+
+    if @pin.save
+      redirect_to @pin, notice: "Successfully created new Pin"
+    else
+      render 'new'
+    end
+  end
+
+  def show
   end
 
   private
     def pin_params
       params.require(:pin).permit(:title, :description)
+    end
+
+    def find_pin
+      @pin = Pin.find(params[:id])
     end
 
 end
