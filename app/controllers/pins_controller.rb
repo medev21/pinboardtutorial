b38152ac3f71c74f7670bd90eb7105208fa5d01e
Martin Ben <mebenav88@gmail.com>
2015-11-04 23:24:17 -0500

    add edit, update, and destroy actions to pin controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -21,6 +21,22 @@ class PinsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Pin was Successfully Updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pin.destroy
+    redirect_to root_path
+  end
+
   private
     def pin_params
       params.require(:pin).permit(:title, :description)
